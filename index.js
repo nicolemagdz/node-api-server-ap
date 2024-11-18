7b3c574b1cb2aabe061ef5821b8be4d1fb9b3e0d
nicolemagdz <nicolemmagdziasz@lewisu.edu>
2024-11-17 22:48:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-var http = require('http');
-var url = require('url');
-var dt = require('./datetime');
+const http = require('http');
+const url = require('url');
+const dt = require('./datetime');
 
-
+// Create a server
 const server = http.createServer((request, response) => {
-    // Write the request to the log. 
-    console.log(request);
+    console.log(`Incoming request: ${request.method} ${request.url}`);
 
-    // Standard Hello World.
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h3>Hello World! --MERNa IaC Server Node.js API on Azure Portal</h3>')
+    response.setHeader('Content-Type', 'application/json');
 
-    // Access funcion from a separate JavaScript module.
-    response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
+    const parsedUrl= url.parse(request.url, true);
 
-    // Show the url. 
-    response.write("req.url="+request.url+"<br><br>");
+    if (parsedUrl.pathname === '/api/date' && request.method === 'GET') {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({ dateTime: dt.myDateTime() }));
+    }
 
-    // Suggest adding something tl the url so that we can parse it. 
-    response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
-    var q = url.parse(request.url, true).query;
-    var txt = q.year + " " + q.month;
-    response.write("txt="+txt);
+    if (parsedUrl.pathname === '/') {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write('<h3>Hello World! --MERNa IaC Server Node.js API on Azure Portal</h3>');
+        response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
+        response.write("req.url="+request.url+"<br><br>");
+        response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
+        response.end('<h3>The End. V2</h3>');
+        return;
+    }
 
-    // Close the response
-    response.end('<h3>The End. V2</h3>');
+    response.writeHead(404, { 'Content-Type': 'application/json'});
+    response.end(JSON.stringify({ error: 'Resource not found' }));
 });
 
 const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+server.listen(port, () => {
+    console.log("Server running at http://localhost:%d", port);
+});
